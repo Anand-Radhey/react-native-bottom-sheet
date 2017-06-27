33161d1f44ec2bdab1c129fcc78e9a90723e786a
Smit Shah <who828@gmail.com>
2017-06-27 11:41:16 +0530

    Forgot to include the import

--- android/src/main/java/com/gnet/bottomsheet/RNBottomSheet.java
+++ android/src/main/java/com/gnet/bottomsheet/RNBottomSheet.java
@@ -15,6 +15,7 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.UiThreadUtil;
 
 import org.json.JSONObject;
 
